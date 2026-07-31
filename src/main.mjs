b79ea178f946627b26a1b890584889647ea96f4f
mathias.brethous@outlook.fr <mathias.brethous@outlook.fr>
2026-07-31 17:58:18 +0200

    remove darwin test

--- src/main.mjs
+++ src/main.mjs
@@ -57,7 +57,7 @@ class ProspectMail {
     app.on("window-all-closed", () => {
       // On macOS it is common for applications and their menu bar
       // to stay active until the user quits explicitly with Cmd + Q
-        app.quit();
+        app.exit(0)
     });
   }
 
